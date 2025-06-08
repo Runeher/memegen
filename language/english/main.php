03be21019ff76aeb73c5ef2baa228f2acdd9bf3c
Runeher <bare1runehauge@gmail.com>
2025-06-08 23:01:20 +0200

    Add files via upload

---
+++ language/english/main.php
@@ -0,0 +1,14 @@
+<?php
+// Main language file for memegen
+
+define('_MD_MEMEGEN_UPLOAD', 'Upload an image to create a meme');
+define('_MD_MEMEGEN_TOP_TEXT', 'Top Text');
+define('_MD_MEMEGEN_BOTTOM_TEXT', 'Bottom Text');
+define('_MD_MEMEGEN_FONT_SIZE', 'Font Size');
+define('_MD_MEMEGEN_TEXT_COLOR', 'Text Color');
+define('_MD_MEMEGEN_GENERATE', 'Generate Meme');
+define('_MD_MEMEGEN_DOWNLOAD', 'Download Meme');
+define('_MD_MEMEGEN_NEW', 'Make Another Meme');
+define('_MD_MEMEGEN_RECENT', 'Recent Memes');
+define('_MD_MEMEGEN_NOTICE', 'Note: We regularly delete memes.');
+define('_MD_MEMEGEN_YOUR_MEME', 'Your meme is ready');
---
+++ language/english/modinfo.php
@@ -0,0 +1,18 @@
+<?php
+// Module Info
+
+define('_MI_MEMEGEN_NAME', 'MemeGen');
+define('_MI_MEMEGEN_DESC', 'Generate memes with your own images and text.');
+
+// Admin Menu
+define('_MI_MEMEGEN_MENU_HOME', 'Home');
+define('_MI_MEMEGEN_MENU_CLEANUP', 'Cleanup');
+
+// Config
+define('_MI_MEMEGEN_THUMB_LIMIT', 'Number of recent memes to show');
+
+define('_MI_MEMEGEN_ENABLE_WATERMARK', 'Enable watermark');
+define('_MI_MEMEGEN_ENABLE_WATERMARK_DESC', 'Show watermark text on memes');
+
+define('_MI_MEMEGEN_WATERMARK_TEXT', 'Watermark text');
+define('_MI_MEMEGEN_WATERMARK_TEXT_DESC', 'Enter the watermark text to appear on memes');
