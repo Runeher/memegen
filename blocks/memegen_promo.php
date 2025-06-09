d21e52ec1b1e160a2aa8b6eed8da4374c46ae23d
Runeher <bare1runehauge@gmail.com>
2025-06-09 18:00:26 +0200

    Add files via upload

---
+++ blocks/memegen_promo.php
@@ -0,0 +1,14 @@
+<?php
+function memegen_promo_show()
+{
+    $block = [];
+
+    $imageUrl = XOOPS_URL . '/modules/memegen/images/memegen_promo3.jpg';
+
+    $link = XOOPS_URL . '/modules/memegen/';
+
+    $block['image'] = $imageUrl;
+    $block['link'] = $link;
+
+    return $block;
+}
