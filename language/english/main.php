2908445cc88e3e62e42017b9e2330ea48248aeb7
Runeher <bare1runehauge@gmail.com>
2025-06-12 14:40:54 +0200

    Add files via upload

--- language/english/main.php
+++ language/english/main.php
@@ -1,14 +1,12 @@
 <?php
 // Main language file for memegen
-
-define('_MD_MEMEGEN_UPLOAD', 'Upload an image to create a meme');
-define('_MD_MEMEGEN_TOP_TEXT', 'Top Text');
-define('_MD_MEMEGEN_BOTTOM_TEXT', 'Bottom Text');
-define('_MD_MEMEGEN_FONT_SIZE', 'Font Size');
-define('_MD_MEMEGEN_TEXT_COLOR', 'Text Color');
-define('_MD_MEMEGEN_GENERATE', 'Generate Meme');
+define('_MD_MEMEGEN_HEADER1', 'Make a Meme');
+define('_MD_MEMEGEN_UPLOAD', 'Upload Image (jpg/png/gif)');
+define('_MD_MEMEGEN_TOP_TEXT', 'Top Text:');
+define('_MD_MEMEGEN_BOTTOM_TEXT', 'Bottom Text:');
+define('_MD_MEMEGEN_FONT_SIZE', 'Font Size:');
+define('_MD_MEMEGEN_FONT_COLOR', 'Font Color:');
+define('_MD_MEMEGEN_ADMIN_SECTION', ':: Administrative Section ::');
 define('_MD_MEMEGEN_DOWNLOAD', 'Download Meme');
-define('_MD_MEMEGEN_NEW', 'Make Another Meme');
-define('_MD_MEMEGEN_RECENT', 'Recent Memes');
-define('_MD_MEMEGEN_NOTICE', 'Note: We regularly delete memes.');
-define('_MD_MEMEGEN_YOUR_MEME', 'Your meme is ready');
+define('_MD_MEMEGEN_ANOTHER_IMAGE', 'Choose Another Image');
+define('_MD_MEMEGEN_PREVIEW', 'PREVIEW');
--- language/english/modinfo.php
+++ language/english/modinfo.php
@@ -5,11 +5,20 @@ define('_MI_MEMEGEN_NAME', 'MemeGen');
 define('_MI_MEMEGEN_DESC', 'Generate memes with your own images and text.');
 
 // Admin Menu
-define('_MI_MEMEGEN_MENU_HOME', 'Home');
-define('_MI_MEMEGEN_MENU_CLEANUP', 'Cleanup');
+define('_MI_MEMEGEN_MENU_HOME', 'MemeGen Watermark Settings');
+define('_MI_MEMEGEN_MENU_SETTINGS', 'Watermark Settings');
+define('_MI_MEMEGEN_MENU_TEXT', 'Watermark Text:');
+define('_MI_MEMEGEN_MENU_ALERT1', 'Watermark is now enabled.');
+define('_MI_MEMEGEN_MENU_ALERT2', 'Watermark is now disabled.');
+define('_MI_MEMEGEN_MENU_ALERT3', 'Watermark text updated.');
+define('_MI_MEMEGEN_MENU_ALERT4', 'Watermark status and text updated.');
+define('_MI_MEMEGEN_ADMIN_ENABLE_WATERMARK', 'Enable Watermark:');
+define('_MI_MEMEGEN_ADMIN_WATERMARK_YES', 'Yes');
+define('_MI_MEMEGEN_ADMIN_WATERMARK_NO', 'No');
+define('_MI_MEMEGEN_ADMIN_WATERMARK_INFO', 'Watermark settings can also be changed under Preferences.');
+define('_MI_MEMEGEN_ADMIN_WATERMARK_SAVE', 'Save Watermark Settings');
 
 // Config
-define('_MI_MEMEGEN_THUMB_LIMIT', 'Number of recent memes to show');
 
 define('_MI_MEMEGEN_ENABLE_WATERMARK', 'Enable watermark');
 define('_MI_MEMEGEN_ENABLE_WATERMARK_DESC', 'Show watermark text on memes');
